af50333d7b6bd48ecd77be69493aba7057fd28c2
Jason Cheatham <jason@jasoncheatham.com>
2020-08-19 15:44:27 -0400

    Style improvements

--- src/Words.tsx
+++ src/Words.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames';
+import { isPangram } from './wordUtil';
 import './Words.css';
 
 interface WordsProps {
@@ -9,11 +11,17 @@ export default function Words(props: WordsProps) {
   const { words } = props;
   return (
     <div className="Words">
-      {words.map((word, i) => (
-        <div key={i} className="Words-word">
-          {word}
-        </div>
-      ))}
+      {words.map((word, i) => {
+        const className = classNames({
+          'Words-word': true,
+          'Words-word-pangram': isPangram(word),
+        });
+        return (
+          <div key={i} className={className}>
+            {word}
+          </div>
+        );
+      })}
     </div>
   );
 }
