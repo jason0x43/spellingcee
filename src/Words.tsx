28431a754b7f661c3c32c99d94b4b03fc78fb213
Jason Cheatham <jason@jasoncheatham.com>
2020-08-21 18:10:06 -0400

    Allow chronologically sorted word list
    fixes #1

--- src/Words.tsx
+++ src/Words.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import classNames from 'classnames';
 import { isPangram } from './wordUtil';
 import './Words.css';
@@ -9,20 +9,35 @@ interface WordsProps {
 
 export default function Words(props: WordsProps) {
   const { words } = props;
+  const [alphabetical, setAlphabetical] = useState<boolean>(false);
+
+  const handleClick = useCallback(() => {
+    setAlphabetical(!alphabetical);
+  }, [setAlphabetical, alphabetical]);
+
+  const displayWords = alphabetical ? [...words].sort() : words;
+
   return (
     <div className="Words">
-      <div className="Words-grid">
-        {words.sort().map((word, i) => {
-          const className = classNames({
-            'Words-word': true,
-            'Words-word-pangram': isPangram(word),
-          });
-          return (
-            <div key={i} className={className}>
-              {word}
-            </div>
-          );
-        })}
+      <div className="Words-controls">
+        <button onClick={handleClick}>
+          {alphabetical ? 'Alphabetical' : 'Chronological'}
+        </button>
+      </div>
+      <div className="Words-grid-wrapper">
+        <div className="Words-grid">
+          {displayWords.map((word, i) => {
+            const className = classNames({
+              'Words-word': true,
+              'Words-word-pangram': isPangram(word),
+            });
+            return (
+              <div key={i} className={className}>
+                {word}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
