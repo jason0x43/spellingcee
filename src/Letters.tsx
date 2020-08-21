7abb3cf1bd5ddb281566335f97b54ac0f1ffe3b3
Jason Cheatham <jason@jasoncheatham.com>
2020-08-21 00:13:01 -0400

    Draw hexagonal tiles

--- src/Letters.tsx
+++ src/Letters.tsx
@@ -8,22 +8,41 @@ interface LettersProps {
   updating?: boolean;
 }
 
-const letterSwapTimeout = 400;
+const letterSwapTimeout = 300;
+const updatingTimeout = 100;
+const tileSize = 100;
+
+// Vertexes of hexagonal tiles
+const points = (function () {
+  const r = tileSize / 2;
+  const n = 6;
+  const p: number[][] = [];
+  for (let i = 0; i < n; i++) {
+    p.push([
+      r + r * Math.cos((2 * Math.PI * i) / n),
+      r + r * Math.sin((2 * Math.PI * i) / n),
+    ]);
+  }
+  return p.map((pt) => `${pt[0]},${pt[1]}`).join(' ');
+})();
 
 export default function Letters(props: LettersProps) {
   const { letters: lettersProp, center } = props;
   const [letters, setLetters] = useState<string[]>([]);
   const [updating, setUpdating] = useState<boolean>(false);
 
-  // Update and re-show letters
+  // Start updating, set timer to swap letters
   useEffect(() => {
     setUpdating(true);
-    const timer = setTimeout(() => {
-      setLetters(lettersProp);
-      setUpdating(false);
-    }, letterSwapTimeout);
+    const timer = setTimeout(() => setLetters(lettersProp), letterSwapTimeout);
     return () => clearTimeout(timer);
   }, [lettersProp]);
+
+  // Re-show letters
+  useEffect(() => {
+    const timer = setTimeout(() => setUpdating(false), updatingTimeout);
+    return () => clearTimeout(timer);
+  }, [letters]);
 
   const className = classNames({
     Letters: true,
@@ -39,9 +58,22 @@ export default function Letters(props: LettersProps) {
         });
 
         return (
-          <div key={letter} className={className}>
-            {letter}
-          </div>
+          <svg
+            key={letter}
+            className={className}
+            viewBox={`0 0 ${tileSize} ${tileSize}`}
+          >
+            <polygon points={points} />
+            <text
+              x="50%"
+              y="50%"
+              dy="3%"
+              dominantBaseline="middle"
+              textAnchor="middle"
+            >
+              {letter}
+            </text>
+          </svg>
         );
       })}
     </div>
