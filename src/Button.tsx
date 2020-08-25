dfd9a5356f6b27c2ae2ee85b00cb354fac17aa06
Jason Cheatham <jason@jasoncheatham.com>
2020-08-25 15:50:00 -0400

    Add some new Button styles, support className

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,18 +3,24 @@ import classNames from 'classnames';
 import './Button.css';
 
 export interface ButtonProps {
+  className?: string;
   size?: 'large' | 'normal' | 'small';
+  type?: 'link' | 'normal';
   children: ReactNode;
   onClick?(): void;
 }
 
 export default function Button(props: ButtonProps) {
-  const { children, onClick, size } = props;
-  const className = classNames({
+  const { className: propClass, children, onClick, size, type } = props;
+  let className = classNames({
     Button: true,
     'Button-small': size === 'small',
-    'Button-large': size === 'large'
+    'Button-large': size === 'large',
+    'Button-link': type === 'link',
   });
+  if (propClass) {
+    className += ` ${propClass}`;
+  }
 
   return <button className={className} onClick={onClick}>{children}</button>;
 }
