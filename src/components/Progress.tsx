4af2522acfdd6155014a161ab4a82e10f14ce4ac
Jason Cheatham <jason@jasoncheatham.com>
2020-11-17 20:36:04 -0500

    Show next threshold on small display
    fixes #18

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,15 +1,16 @@
 import React, { FunctionComponent, useMemo } from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 import { getProgressLabel, getProgressThresholds } from '../wordUtil';
-import {AppState} from '../store';
+import { AppState } from '../store';
 import './Progress.css';
 
 const Progress: FunctionComponent = () => {
-  const game = useSelector((state: AppState) => state.game)
+  const game = useSelector((state: AppState) => state.game);
   const { score, maxScore } = game;
   const thresholds = useMemo(() => getProgressThresholds(maxScore), [maxScore]);
   const label = getProgressLabel(score, maxScore);
+  const currentThreshold = thresholds.findIndex((item) => item.label === label);
 
   return (
     <div className="Progress">
@@ -19,7 +20,8 @@ const Progress: FunctionComponent = () => {
           const className = classNames({
             'Progress-threshold': true,
             'Progress-threshold-met': score >= entry.threshold,
-            'Progress-threshold-current': label === entry.label,
+            'Progress-threshold-current': i === currentThreshold,
+            'Progress-threshold-next': i === currentThreshold + 1,
           });
           return (
             <li key={i} className={className}>
