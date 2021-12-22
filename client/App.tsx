9d112b361026336ac66ec1694474ca8ea1a4e5c7
Jason Cheatham <jason@jasoncheatham.com>
2021-12-22 16:17:25 -0500

    Use reducer for main App state

--- client/App.tsx
+++ client/App.tsx
@@ -3,7 +3,7 @@
 /// <reference lib="deno.unstable" />
 
 import { classNames } from "./util.ts";
-import { React, useCallback, useEffect, useState } from "./deps.ts";
+import { React, useCallback, useEffect, useReducer, useState } from "./deps.ts";
 import AppError from "./AppError.ts";
 import Button from "./components/Button.tsx";
 import Input from "./components/Input.tsx";
@@ -27,136 +27,218 @@ interface LoggedInProps {
   gameData: GameData[];
 }
 
-const LoggedIn: React.FC<LoggedInProps> = (props) => {
-  const { user } = props;
-  const [inputDisabled, setInputDisabled] = useState(false);
-  const [error, setError] = useState<Error | undefined>();
-  const [warning, setWarning] = useState<string | undefined>();
-  const [letters, setLetters] = useState<string[]>([...props.game.key]);
-  const [letterMessage, setLetterMessage] = useState<
-    { type: "normal" | "good" | "bad"; message: string } | undefined
-  >();
-  const [wordListExpanded, setWordListExpanded] = useState(false);
-  const [toastMessage, setToastMessage] = useState<
-    { type: "normal" | "good" | "bad"; message: string } | undefined
-  >();
-  const [inputValue, setInputValue] = useState<string[]>([]);
-  const [validLetters, setValidLetters] = useState<string[]>([]);
-  const [words, setWords] = useState<WordsType>(() =>
-    props.words.reduce((allWords, word) => {
+interface AppState {
+  user: User;
+  inputDisabled: boolean;
+  error: Error | undefined;
+  warning: string | undefined;
+  letters: string[];
+  letterMessage:
+    | { type: "normal" | "good" | "bad"; message: string }
+    | undefined;
+  wordListExpanded: boolean;
+  toastMessage:
+    | { type: "normal" | "good" | "bad"; message: string }
+    | undefined;
+  inputValue: string[];
+  words: WordsType;
+  validWords: string[];
+  game: Game;
+  games: Game[];
+  gameData: { [gameId: number]: GameData };
+}
+
+function initState(props: LoggedInProps): AppState {
+  return {
+    user: props.user,
+    inputDisabled: false,
+    error: undefined,
+    warning: undefined,
+    letters: [...props.game.key],
+    letterMessage: undefined,
+    wordListExpanded: false,
+    toastMessage: undefined,
+    inputValue: [],
+    words: props.words.reduce((allWords, word) => {
       allWords[word.word] = word;
       return allWords;
-    }, {} as WordsType) ?? {}
-  );
-  const [validWords, setValidWords] = useState<string[]>([]);
-  const [game, setGame] = useState(props.game);
-  const [games, setGames] = useState(props.games);
-  const [gameData, setGameData] = useState(() =>
-    props.gameData.reduce((allData, data) => {
+    }, {} as WordsType) ?? {},
+    validWords: [],
+    game: props.game,
+    games: props.games,
+    gameData: props.gameData.reduce((allData, data) => {
       allData[data.gameId] = data;
       return allData;
-    }, {} as { [id: number]: GameData })
+    }, {} as { [id: number]: GameData }),
+  };
+}
+
+type AppStateAction =
+  | { type: "scrambleLetters" }
+  | {
+    type: "setLetterMessage";
+    payload: NonNullable<AppState["letterMessage"]>;
+  }
+  | { type: "clearLetterMessage" }
+  | { type: "clearWarning" }
+  | { type: "clearInput" }
+  | { type: "setWordListExpanded"; payload: boolean }
+  | { type: "enableInput" }
+  | { type: "disableInput" }
+  | { type: "addInput"; payload: string }
+  | { type: "addWord"; payload: GameWord }
+  | { type: "deleteInput" };
+
+function updateState(state: AppState, action: AppStateAction): AppState {
+  switch (action.type) {
+    case "scrambleLetters":
+      return {
+        ...state,
+        letters: [state.letters[0], ...permute(state.letters.slice(1))],
+      };
+    case "setLetterMessage":
+      return {
+        ...state,
+        letterMessage: action.payload,
+        inputDisabled: true,
+      };
+    case "clearLetterMessage":
+      return { ...state, letterMessage: undefined };
+    case "clearWarning":
+      return { ...state, warning: undefined };
+    case "setWordListExpanded":
+      return { ...state, wordListExpanded: action.payload };
+    case "addInput":
+      return {
+        ...state,
+        inputValue: [...state.inputValue, action.payload],
+      };
+    case "enableInput":
+      return { ...state, inputDisabled: false };
+    case "disableInput":
+      return { ...state, inputDisabled: true };
+    case "deleteInput":
+      return {
+        ...state,
+        inputValue: state.inputValue.slice(0, state.inputValue.length - 1),
+      };
+    case "clearInput":
+      return { ...state, inputValue: [] };
+    case "addWord":
+      return {
+        ...state,
+        words: {
+          ...state.words,
+          [action.payload.word]: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+}
+
+interface AppWordsProps {
+  gameData: GameData;
+  words: WordsType;
+  user: User;
+  dispatch: React.Dispatch<AppStateAction>;
+}
+
+const AppWords: React.FC<AppWordsProps> = (props) => {
+  const { gameData, words, user, dispatch } = props;
+  return (
+    <div className="App-words">
+      <Progress gameData={gameData} />
+      <Words
+        words={words}
+        gameData={gameData}
+        user={user}
+        setWordListExpanded={(expanded) =>
+          dispatch({ type: "setWordListExpanded", payload: expanded })}
+      />
+    </div>
   );
-  const isVertical = useVerticalMediaQuery();
+};
 
-  const scrambleLetters = () => {
-    if (letters) {
-      setLetters([letters[0], ...permute(letters.slice(1))]);
+async function submitWord(
+  state: AppState,
+  dispatch: React.Dispatch<AppStateAction>,
+) {
+  const { game, inputValue, user } = state;
+  try {
+    const newWord = await addWord({
+      userId: user.id,
+      gameId: game.id,
+      word: inputValue.join(""),
+    });
+    dispatch({ type: "addWord", payload: newWord });
+  } catch (error) {
+    console.error(error);
+  }
+  dispatch({ type: "clearInput" });
+}
+
+function createKeyPressHandler(
+  state: AppState,
+  dispatch: React.Dispatch<AppStateAction>,
+) {
+  return (event: KeyboardEvent) => {
+    const { game, inputDisabled, user } = state;
+
+    if (inputDisabled) {
+      return;
     }
-  };
 
-  const addInput = (char: string) => {
-    setInputValue([...inputValue, char]);
-  };
-
-  const deleteInput = () => {
-    setInputValue(inputValue.slice(0, inputValue.length - 1));
-  };
-
-  const submitWord = async () => {
-    try {
-      if (game) {
-        const newWord = await addWord({
-          userId: user.id,
-          gameId: game.id,
-          word: inputValue.join(""),
-        });
-        setWords({
-          ...words,
-          [newWord.word]: newWord,
-        });
-      }
-    } catch (error) {
-      console.error(error);
+    // Ignore meta/control keys
+    if (event.metaKey) {
+      return;
     }
-    setInputValue([]);
-  };
 
-  const handleLetterMessageHidden = useCallback(() => {
-    setInputValue([]);
-    setInputDisabled(false);
-    setLetterMessage(undefined);
-  }, []);
+    const { key } = event;
 
-  // TODO: update inputDisable when setting letterMessage
-  useEffect(() => {
-    if (letterMessage) {
-      setInputDisabled(true);
-    }
-  }, [letterMessage]);
-
-  // Add event listeners
-  useEffect(() => {
-    // Handle a general keypress event
-    const handleKeyPress = (event: KeyboardEvent) => {
-      if (inputDisabled) {
-        return;
-      }
-
-      // Ignore meta/control keys
-      if (event.metaKey) {
-        return;
-      }
-
-      const { key } = event;
-
-      if (key.length > 1) {
-        if (key === "Backspace" || key === "Delete") {
-          deleteInput();
-        } else if (key === "Enter") {
-          if (game) {
-            submitWord();
-          }
+    if (key.length > 1) {
+      if (key === "Backspace" || key === "Delete") {
+        dispatch({ type: "deleteInput" });
+      } else if (key === "Enter") {
+        if (game) {
+          submitWord(state, dispatch);
         }
-      } else if (key === " ") {
-        scrambleLetters();
-      } else if ((key >= "a" && key <= "z") || (key >= "A" && key <= "Z")) {
-        addInput(event.key);
       }
-    };
+    } else if (key === " ") {
+      dispatch({ type: "scrambleLetters" });
+    } else if ((key >= "a" && key <= "z") || (key >= "A" && key <= "Z")) {
+      dispatch({ type: "addInput", payload: event.key });
+    }
+  };
+}
 
+const LoggedIn: React.FC<LoggedInProps> = (props) => {
+  const [state, dispatch] = useReducer(updateState, props, initState);
+  const isVertical = useVerticalMediaQuery();
+  const {
+    error,
+    game,
+    gameData,
+    games,
+    inputDisabled,
+    inputValue,
+    letterMessage,
+    letters,
+    toastMessage,
+    user,
+    warning,
+    wordListExpanded,
+    words,
+  } = state;
+
+  useEffect(() => {
+    const handleKeyPress = createKeyPressHandler(state, dispatch);
     globalThis.addEventListener("keydown", handleKeyPress);
 
     return () => {
       globalThis.removeEventListener("keydown", handleKeyPress);
     };
   }, [inputDisabled, inputValue, game]);
-
-  const handleHideWarning = useCallback(() => {
-    setWarning(undefined);
-  }, []);
-
-  const renderWords = () => (
-    <div className="App-words">
-      {game && gameData?.[game.id] && <Progress gameData={gameData[game.id]} />}
-      <Words
-        words={words}
-        gameData={gameData[game.id]}
-        user={user}
-        setWordListExpanded={setWordListExpanded}
-      />
-    </div>
-  );
 
   // If there was an error, display an error message rather than the normal UI
   if (error) {
@@ -195,20 +277,28 @@ const LoggedIn: React.FC<LoggedInProps> = (props) => {
             "App-words-expanded": wordListExpanded,
           })}
         >
-          {isVertical && renderWords()}
+          {isVertical && (
+            <AppWords
+              gameData={gameData[game.id]}
+              words={words}
+              user={user}
+              dispatch={dispatch}
+            />
+          )}
 
           <div className="App-letters">
-            <Input value={inputValue} validLetters={validLetters} />
+            <Input value={inputValue} validLetters={letters} />
             <Letters
               disabled={inputDisabled}
-              addInput={addInput}
+              addInput={(letter) =>
+                dispatch({ type: "addInput", payload: letter })}
               letters={letters}
             />
             <div className="App-letters-controls">
               <Button
                 onClick={() => {
                   if (!inputDisabled) {
-                    deleteInput();
+                    dispatch({ type: "deleteInput" });
                   }
                 }}
               >
@@ -217,23 +307,36 @@ const LoggedIn: React.FC<LoggedInProps> = (props) => {
               <Button
                 onClick={() => {
                   if (!inputDisabled) {
-                    scrambleLetters();
+                    dispatch({ type: "scrambleLetters" });
                   }
                 }}
               >
                 Mix
               </Button>
-              <Button onClick={() => submitWord()}>Enter</Button>
+              <Button onClick={() => submitWord(state, dispatch)}>
+                Enter
+              </Button>
             </div>
             <Message
               message={letterMessage?.message}
               type={letterMessage?.type}
               visibleTime="normal"
-              onHide={handleLetterMessageHidden}
+              onHide={() => {
+                dispatch({ type: "clearInput" });
+                dispatch({ type: "enableInput" });
+                dispatch({ type: "clearLetterMessage" });
+              }}
             />
           </div>
 
-          {!isVertical && renderWords()}
+          {!isVertical && (
+            <AppWords
+              gameData={gameData[game.id]}
+              words={words}
+              user={user}
+              dispatch={dispatch}
+            />
+          )}
 
           <Message
             message={toastMessage?.message}
@@ -243,7 +346,10 @@ const LoggedIn: React.FC<LoggedInProps> = (props) => {
       </>
 
       {warning && (
-        <Modal type="warning" onHide={handleHideWarning}>
+        <Modal
+          type="warning"
+          onHide={() => dispatch({ type: "clearWarning" })}
+        >
           {warning}
         </Modal>
       )}
