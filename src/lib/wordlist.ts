8a6dd7d17e67e17d317ed0a7aae5318522ba660b
Jason Cheatham <jason@jasoncheatham.com>
2022-07-04 19:53:39 -0400

    Filter more words
    - no 's' words
    - no words with both 'e' and 'r'

--- src/lib/wordlist.ts
+++ src/lib/wordlist.ts
@@ -6,34 +6,30 @@ import words50 from 'wordlist-english/english-words-50.json';
 import { random } from './util';
 import { isPangram } from './words';
 
-const longWords = (word: string) => word.length >= 4;
-const longWords10 = words10.filter(longWords);
-const longWords20 = words20.filter(longWords);
-const longWords35 = words35.filter(longWords);
-const longWords40 = words40.filter(longWords);
-const longWords50 = words50.filter(longWords);
+const filterWords = (word: string) =>
+  word.length >= 4 &&
+  !word.includes('s') &&
+  !(word.includes('e') && word.includes('r'));
+
+const filteredWords10 = words10.filter(filterWords);
+const filteredWords20 = words20.filter(filterWords);
+const filteredWords35 = words35.filter(filterWords);
+const filteredWords40 = words40.filter(filterWords);
+const filteredWords50 = words50.filter(filterWords);
 
 export const wordlist = [
-  ...longWords10,
-  ...longWords20,
-  ...longWords35,
-  ...longWords40,
-  ...longWords50
-];
-
-const blocks = [
-  longWords10.length,
-  longWords20.length,
-  longWords35.length,
-  longWords40.length,
-  longWords50.length
+  ...filteredWords10,
+  ...filteredWords20,
+  ...filteredWords35,
+  ...filteredWords40,
+  ...filteredWords50
 ];
 
 /**
  * Linearly search the word list for a pangram, starting from a random position.
  */
 export function getRandomPangram(): string {
-  const maxIndex = blocks[0] + blocks[1] + blocks[2] + blocks[3] + blocks[4];
+  const maxIndex = wordlist.length - 1;
   const startIndex = random(maxIndex);
   for (let i = 0; i < wordlist.length; i++) {
     const index = (i + startIndex) % wordlist.length;
