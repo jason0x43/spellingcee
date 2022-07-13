69d693f8f18cf184e5937b5c465f77bc947ae3dd
Jason Cheatham <jason@jasoncheatham.com>
2022-07-13 14:12:34 -0400

    Remove log statement

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = async ({ locals }) => {
   const user = locals.session?.user;
-  console.log('user:', user);
   return {
     status: 302,
     headers: {
